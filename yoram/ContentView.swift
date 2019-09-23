61d4ecfe89a3245d98747484e9dc5357ae252c60
charlie <mals93@naver.com>
2019-09-23 23:08:44 +0900

    Initial Commit

---
+++ yoram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  yoram
+//
+//  Created by 박천명 on 23/09/2019.
+//  Copyright © 2019 Charile. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
